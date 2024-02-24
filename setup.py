c4ff9067ee06d0f6ee820a470789ffa5d9575f3c
Edem-Gbafa <bells.edem@gmail.com>
2024-02-24 13:35:38 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name ='mypackage',
+    version = '0.1',
+    packages = find_packages(exclude=['test*']),
+    license='MIT',
+    description='EDSA my first Python pagckage',
+    long_description=open('README').open(),
+    install_requires =['numpy'],
+    url='https://github.com /<Edem-Gbafa>/<mypackage>',
+    author='<Edem Gbafa>',
+    author_email='<bells.edem@gmail.com>'
+)
